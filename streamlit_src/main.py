b7d893a843cb40735c252ad3c7b354b2d2ef2d0e
hibana2077 <hibana2077@gmail.com>
2023-01-01 14:46:03 +0800

    2023-01-01

--- streamlit_src/main.py
+++ streamlit_src/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2022-12-23 15:45:40
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-01-01 14:38:59
+LastEditTime: 2023-01-01 14:45:59
 FilePath: \OOP-independent-study\streamlit_src\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -109,7 +109,7 @@ def data_process(model_name:str , exchange:ccxt.Exchange, symbol , timeframe):
     if model_name.startswith('RFCV'):
         data_4_RFCseries(df)
         
-
+#HI this is a test
 
 def model():
     map_table = {
